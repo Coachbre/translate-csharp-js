d0779f1ef6676a29a9742324f1f76adaa1b7fa56
Coachbre <brekonan1@gmail.com>
2021-05-24 13:06:01 -0400

    completed C# to JS challenge

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,1 +1,19 @@
 // Put your code here
+
+const placeNames = ["Nashville", "Hong Kong", "The back yard", "Earth",
+    "London", "The mall", "Ryman Auditorium", "The Great Wall of China"];
+
+    console.log('All Place Names');
+
+    for (let eachPlace of placeNames) {
+        console.log(eachPlace);
+    }
+
+    console.log(' ')
+
+const TheNames = placeNames.filter((n) => n.startsWith("The"));
+
+console.log("'The' Place Names");
+ for (let eachTheName of TheNames) {
+     console.log(eachTheName);
+ }
