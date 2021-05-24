b9c38b8fab892e8e1a7e6b4841eb294b55895a9c
Coachbre <brekonan1@gmail.com>
2021-05-24 18:53:29 -0400

    completed translations

--- rolling-dice/js/rollingDice/main.js
+++ rolling-dice/js/rollingDice/main.js
@@ -1,1 +1,21 @@
 // Put your code here
+
+console.log("Let's roll some dice, baby!");
+console.log("---------------------------");
+
+
+
+const getRoll = () => {
+    for (let i = 0; i < 10; i++) {
+
+        let die1 = (Math.floor(Math.random() * 6) + 1);
+        let die2 = (Math.floor(Math.random() * 6) + 1);
+        let message = (`${die1} + ${die2} == ${die1 + die2}`);
+        if (die1 == die2) {
+            console.log(message += " DOUBLES!");
+        } else {
+            console.log(message);
+        }
+    }
+}
+getRoll();
