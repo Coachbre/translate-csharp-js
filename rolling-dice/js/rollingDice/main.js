014c2d4b734accbb1934c7c8fe768f239121678c
Coachbre <brekonan1@gmail.com>
2021-05-24 19:01:46 -0400

    Changed code based on instructions

--- rolling-dice/js/rollingDice/main.js
+++ rolling-dice/js/rollingDice/main.js
@@ -3,13 +3,17 @@
 console.log("Let's roll some dice, baby!");
 console.log("---------------------------");
 
+const getRoll = (min, max) => {
+    min = Math.ceil(1);
+    max = Math.floor(6);
+    return Math.floor(Math.random() * 6) + 1;
+}
 
-
-const getRoll = () => {
+const results = () => {
     for (let i = 0; i < 10; i++) {
 
-        let die1 = (Math.floor(Math.random() * 6) + 1);
-        let die2 = (Math.floor(Math.random() * 6) + 1);
+        let die1 = getRoll()
+        let die2 = getRoll()
 
         let message = (`${die1} + ${die2} == ${die1 + die2}`);
 
@@ -22,4 +26,4 @@ const getRoll = () => {
         }
     }
 }
-getRoll();
+results();
