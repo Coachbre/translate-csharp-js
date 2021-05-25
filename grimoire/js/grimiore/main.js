42caa6e1f3d4488ec3457b27c63573da49a6cf3c
Coachbre <brekonan1@gmail.com>
2021-05-24 20:48:35 -0400

    started translation but unsure of c# functions/purpose

--- grimoire/js/grimiore/main.js
+++ grimoire/js/grimiore/main.js
@@ -1,1 +1,32 @@
 // Put your code here
+
+console.log("Do you believe in magic?");
+console.log("------------------------");
+
+const spell = [
+    {
+        name:"Turn enemy into a newt",
+        isEvil: true,
+        energyRequired = 5.1
+    },
+    {
+        name: "Conjure some gold for a local charity",
+        isEvil: false,
+        energyRequired = 2.99
+    },
+    {
+        name: "Give a deaf person the ability to heal",
+        isEvil: true,
+        energyRequired = 12.2
+    },
+    {
+        name: "Make yourself emperor of the universe",
+        isEvil: true,
+        energyRequired = 100.0
+    },
+       {
+           name: "Convince your relatives your political views are correct",
+           isEvil: false,
+           energyRequired = 2921.5
+       }
+]
