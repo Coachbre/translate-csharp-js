9fd4e349f76699190ce113584688c79aa7060c3d
Coachbre <brekonan1@gmail.com>
2021-05-24 21:19:51 -0400

    completed translation based on output // not too sure of actual c# code

--- grimoire/js/grimiore/main.js
+++ grimoire/js/grimiore/main.js
@@ -3,30 +3,47 @@
 console.log("Do you believe in magic?");
 console.log("------------------------");
 
-const spell = [
+const allSpells = [
     {
-        name:"Turn enemy into a newt",
-        isEvil: true,
-        energyRequired = 5.1
+        name: "Turn enemy into a newt",
+        isEvil: true
     },
     {
         name: "Conjure some gold for a local charity",
-        isEvil: false,
-        energyRequired = 2.99
+        isEvil: false
     },
     {
         name: "Give a deaf person the ability to heal",
-        isEvil: true,
-        energyRequired = 12.2
+        isEvil: false
     },
     {
         name: "Make yourself emperor of the universe",
-        isEvil: true,
-        energyRequired = 100.0
+        isEvil: true
     },
-       {
-           name: "Convince your relatives your political views are correct",
-           isEvil: false,
-           energyRequired = 2921.5
-       }
+    {
+        name: "Convince your relatives your political views are correct",
+        isEvil: false
+    }
 ]
+
+const goodSpells = () => {
+    console.log("Good Book");
+    for (let eachSpell of allSpells) {
+        if (eachSpell.isEvil == false) {
+            console.log(`${eachSpell.name}`)
+        }
+    }
+}
+
+const evilSpells = () => {
+    console.log("Evil Book");
+    for (let eachSpell of allSpells) {
+        if (eachSpell.isEvil == true) {
+            console.log(`${eachSpell.name}`)
+        }
+    }
+}
+
+goodSpells();
+console.log(" ")
+evilSpells();
